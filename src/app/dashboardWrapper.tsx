d30eb255f0f959901fdcfe23e715b88dcf0e7ad1
zendayat <nmapzendaya@gmail.com>
2024-12-19 15:24:00 +0300

    update: light and dark mode configured

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const DashboardWrapper = ({ children} : {children: React.ReactNode}) => {
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>Sidebar
+    <div className={`dark flex bg-gray-50 text-gray-900 w-full min-h-screen`}>Sidebar
     <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-200`}>
     {children}
     </main>
